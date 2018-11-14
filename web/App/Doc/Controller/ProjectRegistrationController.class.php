aac72fa4a2ec499bcb4b2bc0414509de501fcd13
songcx <prosong.to@foxmail.com>
2018-11-14 19:11:37 +0800

    add
    Signed-off-by: songcx <prosong.to@foxmail.com>

---
+++ web/App/Doc/Controller/ProjectRegistrationController.class.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Doc\Controller;
+use Think\Controller;
+use Doc\Model\ProjectRegistrationModel;
+
+/**
+*项目管理控制器
+*@author song.chaoxu
+*2018.11.14
+*/
+class ProjectRegistrationController extends BaseController {
+
+	/**
+     * 输出首页
+     * @author song.chaoxu
+     * 2018.11.14
+     */
+    public function index()
+    {
+
+    	$this->display();
+    	
+    }
+
+	/**
+	 * 现有项目列表接口
+	 * @author song.chaoxu
+	 * 2018.11.14
+	 */
+    public function projectRecord()
+    {
+
+    	//区域
+    	// $area_id = I('area_id');
+      
+    	//状态
+	 	// $status_id = I('status_id');
+       
+	 	//关键词
+	 	// $kw = I('keywords');
+
+	 	
+	 	//项目列表
+    	// $res=$this->res=
+    	// ProjectManagementModel::projectList($area_id,
+    	// 	$status_id,$kw);
+
+        
+        
+	 	//以下是所有下拉框列表
+    	//所有区域
+    	$areaRes=$this->area=
+    	ProjectRegistrationModel::areaList($province_id);
+    	
+
+        //所有行业
+        $industryResult=$this->projectindustry=
+        ProjectRegistrationModel::projectIndustryList();
+
+        //项目密级别
+        $rank=$this->rank=
+        ProjectRegistrationModel::projectRankList();
+
+
+  
+    	$temp = array();
+
+    	foreach ($res as $v) 
+    	{
+
+			if(!is_array($temp[$v['pid']]))
+            {
+				$temp[$v['pid']]['id'] = (int)$v['pid'];
+				$temp[$v['pid']]['name'] = $v['pname'];
+				$temp[$v['pid']]['child'] = array();
+			}
+
+				$child['id'] = (int)$v['cid'];
+				$child['name'] = $v['cname'];
+				$child['area'] = $v['area'];
+				$child['industry'] = $v['pname'];
+				$child['charge'] = $v['charge'];
+				
+                //统计项目成员数量
+                $member_num=$this->member_num=
+                ProjectManagementModel::
+                member_numCount((int)$v['cid']);
+                $member_num = $member_num[0]['member_num'];
+
+                $child['member_num'] = $member_num;
+				$child['start_time'] = $v['start_time'];
+				
+				if($v['status'] == "完成验收")
+				{
+					
+					$child['end_time'] = $v['end_time'];
+				}
+				else
+				{
+					$child['end_time'] = "今";
+				}
+				$child['status'] = $v['status'];
+				$child['rate'] = $v['rate'];
+
+				array_push($temp[$v['pid']]['child'], $child);
+
+		}
+    	
+    	$final['area'] = $areaRes;
+        $final['industryResult'] = $industryResult;
+        $final['rank'] = $rank;
+    	
+      	$this->Response(0,$final,'');
+
+    }
+
+
+    /**
+	 * 添加项目接口
+	 * @author fang.yu
+	 * 2018.8.15
+	 */
+    public function projectAdd()
+    {
+
+		//项目名称
+    	$name = I('name');
+
+    	//行业分类id
+    	$industry_id = I('industry_id');
+
+    	//客户类型
+    	$customer_type_id = I('customer_type_id');
+
+    	//项目类型id
+    	$project_type_id = I('project_type_id');
+
+    	//所在区域id
+    	$area_id = I('area_id');
+
+        //项目负责人
+        $charge = I('charge');
+
+    	//所在地址
+    	$address = I('address');
+
+    	//经度
+    	$longitude = I('longitude');
+
+    	//纬度
+    	$latitude = I('latitude');
+
+    	//开始时间
+		$start_time = I('start_time');
+
+
+		$res=$this->res=
+    	ProjectManagementModel::projectAdd($name,
+    	$project_type_id,$industry_id,
+        $customer_type_id,$area_id,$charge,
+    	$address,$longitude,$latitude,$start_time);
+
+        $this->redirect('ProjectManagement/Manage');
+    	
+    }
+
+
+
+}
---
+++ web/App/Doc/Model/ProjectRegistrationModel.class.php
@@ -0,0 +1,138 @@
+<?php
+namespace Doc\Model;
+mysql_query("SET NAMES UTF8"); 
+
+class ProjectRegistrationModel
+{
+
+
+	/**
+	 * 查询现有项目列表
+	 * @author song.chaoxu
+	 * 2018.11.14
+	 */
+	 public function projectList($area_id,$status_id,$kw)
+	 {
+	 	
+	 	$sql = "SELECT i.id as pid,
+	 	i.name as pname,
+	 	pm.id as cid,pm.name as cname,
+	 	a.name as area,
+	 	pm.charge,pm.member_num,
+	 	pm.start_time,
+	 	pm.end_time,s.name as status,
+	 	pm.progress_rate as rate
+		from ProjectManagement pm 
+		join project_select i 
+		on pm.industry_id  = i.id 
+		join area a 
+		on pm.area_id = a.id
+		join `project_status` s 
+		on pm.status_id = s.id";
+			
+		//根据传来的不同条件进行搜索	 
+		if(!empty($area_id) && empty($status_id) && empty($kw))
+		{
+			$sql.=" where a.id = '$area_id' ORDER BY pm.id ";
+		}
+		if(empty($area_id)&&!empty($status_id)&&empty($kw))
+		{
+			$sql.=" where s.id = '$status_id' ORDER BY pm.id ";
+		}
+		if(empty($area_id)&&empty($status_id)&&!empty($kw))
+		{
+			$sql.=" where pm.name like '%$kw%' ORDER BY pm.id ";
+		}
+
+
+	 	$res = M()->query($sql);
+
+        return  $res;
+
+	 }
+
+
+	 /**
+	 * 区域下拉框
+	 * @author fang.yu
+	 * 2018.8.14
+	 */
+	  public function areaList()
+     {
+     	$provinceSql = "SELECT id as pid,
+ 				name as pname from area 
+     			where parent_id = 0";
+
+     	$province = M()->query($provinceSql);
+		$area['province'] = $province;
+        return  $area;
+
+     }
+
+     /**
+	 * 行业下拉框
+	 * @author song.chaoxu
+	 * 2018.11.14
+	 */
+	  public function projectIndustryList()
+     {
+
+     	// echo "projectIndustryList is OK";
+     	$sql = "SELECT * FROM app_industry;";
+
+     	$industry = M()->query($sql);
+
+        return  $industry;
+
+     }
+
+    /**
+	 * 行业下拉框
+	 * @author song.chaoxu
+	 * 2018.11.14
+	 */
+	  public function projectRankList()
+     {
+
+     	// echo "projectIndustryList is OK";
+     	$sql = "SELECT * FROM app_project_rank;";
+
+     	$industry = M()->query($sql);
+
+        return  $industry;
+
+     }
+
+
+
+
+     /**
+	 * 添加项目
+	 * @author fang.yu
+	 * 2018.8.15
+	 */
+    public function projectAdd($name,
+    	$project_type_id,$industry_id,
+        $customer_type_id,$area_id,$charge,
+    	$address,$longitude,$latitude,$start_time)
+    {
+    	  $sql="insert into ProjectManagement
+    	  (name,project_type_id,industry_id,
+    	  customer_type_id,area_id,charge,status_id,
+    	  progress_rate,detailedAddress,longitude,
+    	  latitude,start_time) 
+    	  values 
+    	  ('$name','$project_type_id',
+    	  '$industry_id','$customer_type_id',
+    	  '$area_id','$charge',1,'0%','$address',
+    	  '$longitude','$latitude','$start_time')";
+
+        $res = M()->execute($sql);
+       	return $res;
+    	 
+    }
+
+
+    
+
+}
