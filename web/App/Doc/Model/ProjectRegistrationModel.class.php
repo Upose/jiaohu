60f9993c03f30a4550a76d5bff77045542c27314
songcx <prosong.to@foxmail.com>
2018-11-21 17:19:28 +0800

    11
    Signed-off-by: songcx <prosong.to@foxmail.com>

--- web/App/Doc/Model/ProjectRegistrationModel.class.php
+++ web/App/Doc/Model/ProjectRegistrationModel.class.php
@@ -164,4 +164,84 @@ class ProjectRegistrationModel
     }
 
 
+
+    /**
+     * 查询现有项目列表
+     * @author song.chaoxu
+     * 2018.11.21
+     */
+     public function projectList($proArea,$proName,$proCode)
+     {
+        
+        // $sql = "SELECT i.id as pid,
+        // i.name as pname,
+        // pm.id as cid,pm.name as cname,
+        // a.name as area,
+        // pm.charge,pm.member_num,
+        // pm.start_time,
+        // pm.end_time,s.name as status,
+        // pm.progress_rate as rate
+        // from ProjectManagement pm 
+        // join project_select i 
+        // on pm.industry_id  = i.id 
+        // join area a 
+        // on pm.area_id = a.id
+        // join `project_status` s 
+        // on pm.status_id = s.id";
+
+        $sql = "
+
+            SELECT
+                `pro_id`,
+                `pro_name`,
+                `type_id`,
+                `industry_id`,
+                `member_id`,
+                `pro_leader`,
+                `pro_members`,
+                `pro_stime`,
+                `pro_etime`,
+                `pro_status`,
+                `pro_schedule`,
+                `pro_address`,
+                `secrecy_grade`,
+                `insert_date`,
+                `create_data`,
+                `pro_longitude`,
+                `pro_latitude`,
+                `pro_enclosure`,
+                `pro_code`,
+                `pro_msg`,
+                `cooperative_unit`,
+                `pro_source`,
+                `division_manager_id`,
+                `pro_division_manager`,
+                `contract_amount`,
+                `pro_introduce` 
+            FROM `app_project`;
+
+        ";
+            
+        echo "$proArea:".$proArea ."————————————$proName".$proName."————————————$proCode".$proCode;
+        //根据传来的不同条件进行搜索  
+        if(!empty($proArea) && empty($proName) && empty($proCode))
+        {
+            $sql.=" where pro_address like '%$proArea%'";
+        }
+        if(empty($proArea)&&!empty($proName)&&empty($proCode))
+        {
+            $sql.=" where pro_naem like '%$proName%'";
+        }
+        if(empty($proArea)&&empty($proName)&&!empty($proCode))
+        {
+            $sql.=" where pro_id like '%$proCode%'";
+        }
+
+        $res = M()->query($sql);
+
+        return  $res;
+
+     }
+
+
 }
