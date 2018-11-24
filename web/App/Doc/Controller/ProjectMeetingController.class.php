a0218e76e2bbfc5c27a988fd764b99e5adf8e92c
songcx <prosong.to@foxmail.com>
2018-11-24 18:39:50 +0800

    123
    Signed-off-by: songcx <prosong.to@foxmail.com>

--- web/App/Doc/Controller/ProjectMeetingController.class.php
+++ web/App/Doc/Controller/ProjectMeetingController.class.php
@@ -122,7 +122,7 @@ class ProjectMeetingController extends BaseController {
             //设置附件上传类型
             // $upload->exts=array('html','htm','jpg', 'gif', 'png', 'jpeg','txt');
             //设置附件上传根目录
-            $upload->rootPath = './Updata/UpdateFile/'; 
+            $upload->rootPath = './Updata/MeetingFile/'; 
             //设置附件上传（子）目录
             $upload->savePath = '';
             $result = $upload->upload();
