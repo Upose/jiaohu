b62a3df39709a8dba6a0dd409a90476ee2e28a22
Sariel <moyue586543@126.com>
2018-11-29 15:46:57 +0800

    样式修改

--- web/App/Common/Common/config.php
+++ web/App/Common/Common/config.php
@@ -8,12 +8,11 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => 'localhost', // 服务器地址
+    'DB_HOST' => '211.144.114.26', // 服务器地址
     'DB_NAME' => 'deliveryapplication', // 数据库名
     'DB_USER' => 'root', // 用户名
-    'DB_PWD' => '',  // 密码
-    'DB_PORT' => '3306', // 端口
+    'DB_PWD' => 'daemon',  // 密码
+    'DB_PORT' => '3307', // 端口
 
     'DEFAULT_MODULE' => 'Doc'
-   
 );
