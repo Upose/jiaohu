f20acfb28d3efe8875d30269a2adbe63dd10a81b
songcx <prosong.to@foxmail.com>
2018-11-29 16:52:11 +0800

    1222222222222
    Signed-off-by: songcx <prosong.to@foxmail.com>

--- web/App/Doc/Model/ProjectMeetingModel.class.php
+++ web/App/Doc/Model/ProjectMeetingModel.class.php
@@ -68,7 +68,7 @@ class ProjectMeetingModel
                               FROM
                                 `app_meeting` m
                               JOIN app_project p ON m.department_id = p.pro_id
-                              WHERE   p.member_id = \"$pMid\"";
+                              WHERE   p.member_id = \"$projectManagerId\"";
 
         if (!empty($pro_id)) {
 
@@ -78,6 +78,8 @@ class ProjectMeetingModel
 
 
         // echo $meetingSql;
+        // echo $meetingCountSql;
+        
         $meetingCount = M()->query($meetingCountSql);
 
      	  $meetingList = M()->query($meetingSql);
