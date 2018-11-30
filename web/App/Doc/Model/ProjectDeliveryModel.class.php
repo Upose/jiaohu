4d5e29e2645702aae548e6ddb333903754289070
songcx <prosong.to@foxmail.com>
2018-11-30 12:22:12 +0800

    123
    Signed-off-by: songcx <prosong.to@foxmail.com>

--- web/App/Doc/Model/ProjectDeliveryModel.class.php
+++ web/App/Doc/Model/ProjectDeliveryModel.class.php
@@ -15,7 +15,7 @@ class ProjectDeliveryModel
       public function projectList($projectManagerId)
      {
         $projectListSql = "SELECT pro_id,pro_name,member_id,pro_leader FROM `app_project` WHERE member_id = \"$projectManagerId\";";
-        $projectList = M()->query($projectList);
+        $projectList = M()->query($projectListSql);
         return  $projectList;
      }
 	 /**
