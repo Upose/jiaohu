9827e31ccbf71f614b3464c39da2dff250616703
wanghuan <wang.huan@datatom.com>
2018-12-03 11:32:11 +0800

    111 Signed-off-by: wanghuan <wang.huan@datatom.com>

--- web/App/Common/Conf/config.php
+++ web/App/Common/Conf/config.php
@@ -8,8 +8,8 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE' => 'mysql', // 数据库类型
+    // 'DB_HOST' => 'localhost', // 服务器地址
     'DB_HOST' => '211.144.114.26', // 服务器地址
-    // 'DB_HOST' => '211.144.114.26', // 服务器地址
     'DB_NAME' => 'deliveryapplication', // 数据库名
     'DB_USER' => 'root', // 用户名
     'DB_PWD' => 'daemon',  // 密码
