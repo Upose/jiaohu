ddab1ae0042839e2ce6257590585ac1dd49338be
songcx <prosong.to@foxmail.com>
2019-01-02 15:59:06 +0800

    123
    Signed-off-by: songcx <prosong.to@foxmail.com>

--- web/App/Doc/Model/ProjectRegistrationModel.class.php
+++ web/App/Doc/Model/ProjectRegistrationModel.class.php
@@ -365,7 +365,7 @@ class ProjectRegistrationModel
      */
       public function dResult()
      {
-        $sql = " SELECT id,deptNmae FROM dm_department";
+        $sql = " SELECT id,deptName FROM dm_department";
         $dResultList = M()->query($sql);
         return  $dResultList;
      }
