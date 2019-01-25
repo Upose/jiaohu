52d30f4ee8e96269b2e2811178ee9681aae4c1c3
song.chaoxu <song.chaoxu@datatom.com>
2019-01-25 15:58:06 +0800

    1

--- web/App/Doc/Model/ItemAcceptanceModel.class.php
+++ web/App/Doc/Model/ItemAcceptanceModel.class.php
@@ -12,4 +12,13 @@ class ItemAcceptanceModel{
             return $e->getMessage();
         }
 	}
+
+	public function acceptList($pro_code,$page,$limit) {
+
+		$res = M('app_project_complete')->where('pro_code='.$pro_code)
+	                                    ->page($page,$limit)
+	                                    ->order('create_date desc')
+										->select();
+		return $res;
+	}
 }
