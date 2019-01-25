6ad96b2d696df9a2a6033167bc05ef82687e5f8d
song.chaoxu <song.chaoxu@datatom.com>
2019-01-25 16:12:15 +0800

    1

--- web/App/Doc/Controller/ItemTestRunController.class.php
+++ web/App/Doc/Controller/ItemTestRunController.class.php
@@ -37,39 +37,7 @@ class ItemTestRunController extends BaseController {
 		//创建人
 		$p['founder_id'] = I('founder_id');
 		//相关附件
-		$p['enclosure'] = '';
-		if ($_FILES) {
-		// echo ($_FILES["file"][size] / 1024)."kb";
-
-        	foreach ($_FILES as $key => $value) {
-	            //实例化上传类
-	            $upload =  new \Think\Upload();
-	            //设置附件上传大小
-	            // $upload->maxSize=3145728;
-	            //保持文件名不变
-	            $upload->saveName = time()."dt".rand(0,10);
-	            //设置附件上传类型
-	            // $upload->exts=array('html','htm','jpg', 'gif', 'png', 'jpeg','txt');
-	            //设置附件上传根目录
-	            $upload->rootPath = './Updata/TestRun/'; 
-	            //设置附件上传（子）目录
-	            $upload->savePath = '';
-	            $result = $upload->upload();
-	            // echo '<pre>';
-	            // var_dump($result);
-	            // echo '</pre>';
-	            // file_put_contents("11114.txt", json_encode($result));
-	            if($result){
-					foreach ($result as $key => $value) {
-					$savename  = $value['savename'];
-					$path  = "/Updata/TestRun/".$value['savepath'];
-					$p['enclosure'] = $newpath = $path.$savename;
-					$href[] = $newpath;
-					// echo $filePath."|_____________________path";
-	            	}
-	            }
-            }
-		}
+		$p['enclosure'] = I('enclosure');
 		$status=$this->status=ItemAcceptanceModel::testRunAdd($p);
             if ($status) {
                 $this->Response(200,$status,'数据新增成功');
